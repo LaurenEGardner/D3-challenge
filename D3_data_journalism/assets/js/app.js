2f9d880ce31a162033e053cfd6fc260927ab518d
LaurenEGardner <heylo.gardner@gmail.com>
2021-01-24 14:10:03 -0800

    forgot to adjust code for file moving

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -24,7 +24,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   //pull data from csv
-d3.csv("./data/data.csv").then(function(stateData) {
+d3.csv("./data.csv").then(function(stateData) {
     console.log(stateData);
 
 stateData.forEach(function(data) {
